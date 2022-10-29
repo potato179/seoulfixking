b758374e81bfcbc272dcbbd2509ccf8598b7aba9
홍은표 <44293278+potato179@users.noreply.github.com>
2022-10-29 23:38:49 +0900

    코드 정리

--- public/js/script.js
+++ public/js/script.js
@@ -14,7 +14,7 @@ let scale, offsetX, offsetY;
 const calculateValues = (() => {
     const w = window.innerWidth;
     const h = window.innerHeight;
-    const offsetValue = Number(getComputedStyle(elem).getPropertyValue('--offset-value'));
+    const offsetValue = Number(getComputedStyle(elem).getPropertyValue("--offset-value"));
 
     offsetX = (w/2) - (elemW/2) - offsetValue;
     offsetY = (h/2) - (elemH/2) - offsetValue;
@@ -26,30 +26,30 @@ const calculateValues = (() => {
 
 
 const openMenu = () => {
-  elem.style.setProperty("--translate-x", `${offsetX}px`);
-  elem.style.setProperty("--translate-y", `-${offsetY}px`);
-  elem.style.setProperty("--scale", scale);
+    elem.style.setProperty("--translate-x", `${offsetX}px`);
+    elem.style.setProperty("--translate-y", `-${offsetY}px`);
+    elem.style.setProperty("--scale", scale);
 }
 const closeMenu = () => {
-  elem.style.setProperty("--scale", 1);
-  elem.style.setProperty("--translate-x", 0);
-  elem.style.setProperty("--translate-y", 0);
+    elem.style.setProperty("--scale", 1);
+    elem.style.setProperty("--translate-x", 0);
+    elem.style.setProperty("--translate-y", 0);
 }
 const animateMenu = () => {
-  open ? openMenu() : closeMenu();
+    open ? openMenu() : closeMenu();
 };
 
 const toggleMenu = () => {
-  open = !open;
-  animateMenu();
-  toggleBtn.classList.toggle('shown');
+    open = !open;
+    animateMenu();
+    toggleBtn.classList.toggle("shown");
 }
 
 const resizeHandler = () => { 
-  window.requestAnimationFrame(() => {
-    calculateValues();
-    animateMenu();
-  });
+    window.requestAnimationFrame(() => {
+        calculateValues();
+        animateMenu();
+    });
 }
 
 calculateValues();
